7f523a1a2e272c8f87dd450b6b75b17de75fce43
djryt <149905317+djryt@users.noreply.github.com>
2023-11-09 19:35:33 -0800

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,4 @@
+calculator = int(input("What is your first number"))
+calculator_2 = int(input("What is the second number"))
+operation = int(input("Do you choose + - * /"))
+print(calculator, operation, calculator_2)
