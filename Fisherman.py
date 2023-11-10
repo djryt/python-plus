996b0f5c2e27e8e42359e070852fbad1bf8bfbb4
djryt <149905317+djryt@users.noreply.github.com>
2023-11-09 19:15:36 -0800

    Update Fisherman.py

--- Fisherman.py
+++ Fisherman.py
@@ -1,3 +1,10 @@
+import random
 fish = int(input("How many kilograms of fish did you get?"))
 dollars = fish*3
-print(f"You have {fish} kilograms {dollars} is how much it costs"
+print(f"You have {fish} kilograms {dollars} is how much it costs")
+
+if dollars >100:
+  gift = random.randint(1,3)
+print(gift)
+
+      
