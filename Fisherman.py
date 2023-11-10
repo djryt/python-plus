920b517565d0678565bb0e9e9fc1ffe38c0d905a
djryt <149905317+djryt@users.noreply.github.com>
2023-11-09 19:28:28 -0800

    Update Fisherman.py

--- Fisherman.py
+++ Fisherman.py
@@ -1,10 +1,17 @@
 import random
 fish = int(input("How many kilograms of fish did you get?"))
 dollars = fish*3
-print(f"You have {fish} kilograms {dollars} is how much it costs")
-
+#1 is 100
 if dollars >100:
   gift = random.randint(1,3)
-print(gift)
+if gift == 1:
+    print("Congratulations you won a fisherman hat")
+    print(f"You have {fish} kilograms ${dollars} is how much it costs")
+elif gift == 2:
+    print("Congratulations you won a boat")
+    print(f"You have {fish} kilograms ${dollars} is how much it costs")
+elif gift == 3:
+    print("Congratulations you won a fishing rod")
+    print(f"You have {fish} kilograms ${dollars} is how much it costs")
 
       
